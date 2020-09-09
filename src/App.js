76679e85c05ad4368a5fcee3c9de2fc19fb18106
yunzaifei <zyunfei86@hotmail.com>
2020-09-09 17:08:10 +0800

    Mapbox Basemap Style

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './assets/stylesheets/App.css';
 import { Map, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
+const MAPBOX_API_KEY = process.env.REACT_APP_MAPBOX_API_KEY;
+const MAPBOX_USERID = process.env.REACT_APP_MAPBOX_USERID;
+const MAPBOX_STYLEID = process.env.REACT_APP_MAPBOX_STYLEID;
+
 function App() {
   return (
     <Map center={[39.907132, 116.386546]} zoom={12}>
       <TileLayer 
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
       />
     </Map>
   );
