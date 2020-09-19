fcb2d6152abf12d11caf8222dc73755af926e75b
yunzaifei <zyunfei86@hotmail.com>
2020-09-19 20:43:53 +0800

    Use Leaflet's Geolocation API

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 // const MAPBOX_STYLEID = process.env.REACT_APP_MAPBOX_STYLEID;
 
 function App() {
-  const myRef = useRef();
+  const mapRef = useRef();
 
   useEffect(() => {
     delete L.Icon.Default.prototype._getIconUrl;
@@ -24,7 +24,7 @@ function App() {
   }, []);
 
   useEffect(() => {
-    const { current = {} } = myRef;
+    const { current = {} } = mapRef;
     const { leafletElement: map } = current;
 
     if (!map) return;
@@ -48,7 +48,6 @@ function App() {
         })
       },
       onEachFeature: (feature = {}, layer) => {
-        console.log('feature', feature, 'layer', layer);
         const { properties = {}, geometry = {} } = feature;
         const { name, delivery, deliveryRadius, tags, phone, address } = properties;
         const { coordinates } = geometry;
@@ -97,16 +96,83 @@ function App() {
       },
     });
     geoJson.addTo(map);
-  }, [myRef])
+  }, [mapRef])
+
+  useEffect(() => {
+    const { current = {} } = mapRef;
+    const { leafletElement: map } = current;
+
+    if (!map) return;
+    map.on('locationfound', handleOnLocationFound);
+
+    return () => {
+      map.off('locationfound', handleOnLocationFound);
+    }
+  }, [mapRef])
+
+  function handleOnLocationFound(event) {
+    const { current = {} } = mapRef;
+    const { leafletElement: map } = current;
+    
+    const latlng = event.latlng;
+    const marker = L.marker(latlng);
+    marker.addTo(map);
+    
+    const radius = event.accuracy;
+    const circle = L.circle(latlng, {
+      radius,
+      color: '#26c6da'
+    });
+
+    circle.addTo(map);
+  }
+
+  function handleOnFindLocation() {
+    const { current = {} } = mapRef;
+    const { leafletElement: map } = current;
+
+    map.locate({
+      setView: true,
+    });
+  }
+
+  function handleOnSetLocation() {
+    const { current = {} } = mapRef;
+    const { leafletElement: map } = current;
+
+    const locationNationalGeographic = [39.90331, 116.410077];
+
+    const marker = L.marker(locationNationalGeographic);
+
+    marker.addTo(map);
+    map.setView(locationNationalGeographic);
+  }
 
   return (
-    <Map ref={myRef} center={[39.90331, 116.410077]} zoom={16}>
-      <TileLayer 
-        // url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
-        url={'https://webrd0{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}'}
-        subdomains={['1', '2', '3', '4']}
-      />
-    </Map>
+    <>
+      <div className="search-actions">
+        <ul>
+          <li>
+            <button onClick={handleOnSetLocation}>
+              默认位置
+            </button>
+          </li>
+          <li>
+            <button onClick={handleOnFindLocation}>
+              我的位置
+            </button>
+          </li>
+        </ul>
+      </div>
+      <Map ref={mapRef} center={[39.90331, 116.410077]} zoom={16}>
+        <TileLayer 
+          // url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
+          url={'https://webrd0{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}'}
+          subdomains={['1', '2', '3', '4']}
+          attribution="&copy;"
+        />
+      </Map>
+    </>
   );
 }
 
